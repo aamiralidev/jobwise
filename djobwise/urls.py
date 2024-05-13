a2dd13bf3ef3bbec35b0e261cac6b01e6687858f
Aamir Ali <aamirali.dev@gmail.com>
2024-05-13 21:14:15 +0500

    ..

--- djobwise/urls.py
+++ djobwise/urls.py
@@ -18,9 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from jobs.urls import job_router
 from . import views
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(job_router.urls)),
-    path('', views.index, name='index')
-]
+    # path('', views.index, name='index'),
+    # path('', views.index, name='index')
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
